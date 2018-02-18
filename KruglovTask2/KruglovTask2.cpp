d97851963a0ff9c7ab6434b1c2b4c70482feb664
DmitriyFokin <fokinds1@gmail.com>
2018-02-18 15:24:12 +0300

    1.2. in work. finished - delete empty before & after punctuation marks

--- KruglovTask2/KruglovTask2.cpp
+++ KruglovTask2/KruglovTask2.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 string ReadFromTextToString(string pathToTextFile);
+string TextFormatting(string originText);
 
 void main()
 {
-	string mainString;
+	string mainString, stringAfterFormat;;
 
 	mainString = ReadFromTextToString("Text.txt");
 	cout << mainString;
+	
+	cout << "\n\nFormat Text:\n\n";
+	stringAfterFormat = TextFormatting(mainString);
+	cout << stringAfterFormat;
 
 	_getch();
 }
@@ -25,9 +30,42 @@ string ReadFromTextToString(string pathToTextFile)
 	while (getline(textFile, stringBuffer))
 		stringWithAllText += stringBuffer;
 	
-		//cout << stringWithAllText;
-
 	textFile.close();
 
 	return stringWithAllText;
+}
+
+string TextFormatting(const string originText)
+{
+	string formatString = originText;
+	int sizeOfString = formatString.length();
+
+	for (int i(0); i < sizeOfString; i++)
+	{
+		if (formatString[i] == ',' || formatString[i] == '.' ||
+			formatString[i] == '?' || formatString[i] == '!' ||
+			formatString[i] == ':' || formatString[i] == ';')
+		{
+			if (formatString[i + 1] != ' ')
+				formatString.insert(i + 1, " ");
+
+			if (formatString[i + 2] == ' ')
+				while (formatString[i + 2] == ' ')
+					formatString.erase(i + 2, 1);
+			
+			int j = i - 1;
+			while (formatString[j] == ' ')
+			{
+				formatString.erase(j, 1);
+				j--;
+				i = j;
+			}
+
+			sizeOfString = formatString.length();
+		}
+	}
+		
+
+
+	return formatString;
 }
